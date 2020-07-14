ccb2d1020fe66e9e4a37f2994169efb1e41e0edc
isaacz <isaac.anselmo.z@gmail.com>
2020-07-13 21:02:19 -0500

    primer version base de datos

---
+++ bd.js
@@ -0,0 +1,54 @@
+const persona={
+    nombre:"",
+    usuario:"",
+    password:"",
+    ubicacion:"",
+    nacimiento:"",
+    esPropietario:true,
+    esCuidador:true,
+    mascotas:array[{
+            nombre:"",
+            raza:"",
+            edad:"",
+            propia:true
+        },
+            {
+                nombre:"mascota2",
+                raza:"",
+                edad:"",
+                propia:false
+            }
+        ]
+
+
+};
+const persona={
+    id:"",
+    nombre:"",
+    usuario:"validar que sea único en la base de datos",
+    password:"",
+    ubicacion:"",
+    nacimiento:"",
+    esPropietario:true,
+    esCuidador:false,
+    calificacion:"es necesario crear una función que realice el cálculo",
+    metodoPago:4,
+    mascotas:array[{
+        nombre:"mascota1",
+        raza:"",
+        edad:"",
+        propietario:"nombreUsuarioPropietario",
+        idPropietario:"",
+        reserva=[{
+            fechaInio:"date" ,
+            hrInicio:"hour",
+            fechafin:"date",
+            hrFin:"hour",
+            cuiador:"nombreUsuarioCuidador",
+            costo:"",
+            idCuidador:""
+         }]
+        
+    }
+    ]
+};
